7a47378849ed70dbded7212676aebe4d3211b049
The1AboveAll <kousikroy110@gmail.com>
2020-05-22 20:27:38 +0530

    added blog content

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from .models import Post
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	return render(request,'blog/post_list.html',{})
+	posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request,'blog/post_list.html',{'posts':posts})
